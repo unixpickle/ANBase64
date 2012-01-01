75dcbb177a6975f3d1191b888da12f5142c339ac
Alex Nichol <alex@Alexs-MacBook-Air.local>
2012-01-01 12:04:04 -0500

    First commit

--- Base64/main.m
+++ Base64/main.m
@@ -3,19 +3,24 @@
 //  Base64
 //
 //  Created by Alex Nichol on 12/31/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Jitsik. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "ANBase64Encoder.h"
+#import "ANBase64Decoder.h"
 
-int main (int argc, const char * argv[])
-{
-
+int main (int argc, const char * argv[]) {
     @autoreleasepool {
+        NSData * strData = [@"the quick brown fox jumps over the lazy dog" dataUsingEncoding:NSASCIIStringEncoding];
+        ANBase64Encoder * encoder = [[ANBase64Encoder alloc] initWithData:strData];
+        NSString * encoded = [encoder encodeBase64Data];
+        ANBase64Decoder * decoder = [[ANBase64Decoder alloc] initWithBase64String:encoded];
+        NSData * decoded = [decoder readAllBytes];
         
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSCAssert([decoded isEqualToData:strData], @"Encode/decode should result in equivalent data.");
+        NSLog(@"Encoded: %@", encoded);
+        NSLog(@"Decoded: %@", decoded);
     }
     return 0;
 }
